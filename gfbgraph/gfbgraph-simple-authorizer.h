c00dcf7e0c66fb9a3c788747537f9ab3d52c2876
Álvaro Peña <alvaropg@gmail.com>
2013-07-17 18:32:38 +0200

    Changed license to LGPL v2.1
    This new license is more common in the GNOME libraries, and the above license came from
    a cut&paste from another project.

--- gfbgraph/gfbgraph-simple-authorizer.h
+++ gfbgraph/gfbgraph-simple-authorizer.h
@@ -3,18 +3,18 @@
  * libgfbgraph - GObject library for Facebook Graph API
  * Copyright (C) 2013 Álvaro Peña <alvaropg@gmail.com>
  *
- * This library is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * GFBGraph is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
- * libgfbgraph is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
+ * GFBGraph is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along
- * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GFBGraph.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef __GFBGRAPH_SIMPLE_AUTHORIZER_H__
