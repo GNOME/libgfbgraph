d5ae6e8d041f360d9ec7a75171b3334cd7da068a
Álvaro Peña <alvaropg@gmail.com>
2013-12-04 23:49:32 +0100

    doc: Just the skeleton to build the documentation

--- autogen.sh
+++ autogen.sh
@@ -8,6 +8,14 @@ test -n "$srcdir" || srcdir=.
 olddir=`pwd`
 cd "$srcdir"
 
+GTKDOCIZE=`which gtkdocize`
+if test -z $GTKDOCIZE; then
+        echo "*** No GTK-Doc found, please install it ***"
+        exit 1
+else
+        gtkdocize || exit $?
+fi
+
 ACLOCAL="${ACLOCAL-aclocal} $ACLOCAL_FLAGS" autoreconf -v -i
 
 cd "$olddir"
