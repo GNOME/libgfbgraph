5df5c4f7043b526f5be713a3e9324cedbe412486
Álvaro Peña <alvaropg@gmail.com>
2015-12-29 10:37:49 +0100

    trivial: added helper file for emacs

---
+++ .dir-locals.el
@@ -0,0 +1,12 @@
+((c-mode
+  (flycheck-clang-include-path "/home/alvaropg/work/git/libgfbgraph/"
+                               "/usr/include/goa-1.0"
+                               "/usr/lib64/goa-1.0/include"
+                               "/usr/include/rest-0.7"
+                               "/usr/include/dbus-1.0"
+                               "/usr/lib64/dbus-1.0/include"
+                               "/usr/include/gio-unix-2.0/"
+                               "/usr/include/glib-2.0"
+                               "/usr/lib64/glib-2.0/include"
+                               "/usr/include/libsoup-2.4"
+                               "/usr/include/libxml2")))
