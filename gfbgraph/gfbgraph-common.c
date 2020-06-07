27fc3f3ca59503b1e7da99d15b74d85d54842e79
Leesoo Ahn <yisooan@fedoraproject.org>
2020-06-07 17:18:39 +0900

    common: graph api version up to 7.0

--- gfbgraph/gfbgraph-common.c
+++ gfbgraph/gfbgraph-common.c
@@ -22,7 +22,7 @@
 
 #include <rest/rest-proxy.h>
 
-#define FACEBOOK_ENDPOINT "https://graph.facebook.com/v2.10"
+#define FACEBOOK_ENDPOINT "https://graph.facebook.com/v7.0"
 
 /**
  * gfbgraph_new_rest_call:
